4e7c8f9240e3c3448de3313fc434a91d8339369c
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-02-22 18:59:10 +0530

    experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,46 @@
 import data from "../static/experienceData.json";
 
+type experienceProps = {
+  id: number;
+  title: string;
+  tenure: string;
+  position: string;
+  description: string;
+  bullets: string[];
+  url: string;
+};
+
 const Experience = () => {
   return (
-    <section id="experience" className="mt-40 text-lg">
+    <section id="experience" className="mt-40 w-3/4">
       <h2 className="text-primary text-2xl font-semibold mb-10 underline underline-offset-4">
         <span>02.&nbsp;</span> Where I've Worked
       </h2>
+
+      {data.map((job: experienceProps) => {
+        return (
+          <>
+            <span className="text-xl font-semibold text-">{job.position} </span>
+            <a
+              href={job.url}
+              className="text-xl text-accented-high font-semibold inline-block mb-8"
+            >
+              @{job.title}
+            </a>
+
+            <p className="mb-4 text-lg">{job.description}</p>
+
+            <ul>
+              {job.bullets.map((bullet) => (
+                <li className="flex" key={job.bullets.indexOf(bullet)}>
+                  <span className="text-accented-high text-xl pr-5">▹</span>
+                  <span className="text-primary text-lg">{bullet}</span>
+                </li>
+              ))}
+            </ul>
+          </>
+        );
+      })}
     </section>
   );
 };
