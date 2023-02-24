b5e54d4ec396de7059f14c13eeabf2e55b74cec6
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-02-24 13:11:27 +0530

    adding copy to clipboard functionality to email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
-import { contacts } from "../static/contactMe";
+import { contacts, tickSVG, copySVG } from "../static/contactMe";
 
 const Contact = () => {
   const [showEmail, setShowEmail] = useState(false);
-  const clickHandler = (openInNewPage: boolean, url: string) => {
+  const [copied, setCopied] = useState(false);
+  const email = "shubham.satyawali.05@gmail.com";
+  const emailClickHandler = (openInNewPage: boolean, url: string) => {
     if (!openInNewPage) {
       setShowEmail((showEmail) => !showEmail);
       return;
     }
     window.open(url);
     setShowEmail(false);
+  };
+  const copyEmailHandler = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(email);
+    setTimeout(() => {
+      setCopied(false);
+    }, 4000);
   };
   return (
     <section id="contact" className="mt-40 text-lg mb-20">
@@ -22,7 +31,7 @@ const Contact = () => {
             <div
               key={title}
               className="px-5 py-4 rounded-lg bg-card duration-300 flex flex-col justify-center items-center gap-2"
-              onClick={() => clickHandler(openInNewPage, url)}
+              onClick={() => emailClickHandler(openInNewPage, url)}
             >
               {svg}
               {title}
@@ -36,19 +45,12 @@ const Contact = () => {
           showEmail ? "block" : "invisible"
         } `}
       >
-        <p className="text-accented-low">Drop an email at 👇:</p>
-        <div className="flex justify-center items-center gap-8">
-          <p className="inline">shubham.satyawali.05@gmail.com</p>
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className="fill-primary w-6 hover:fill-accented-low"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M7 4V2h10v2h3.007c.548 0 .993.445.993.993v16.014a.994.994 0 0 1-.993.993H3.993A.994.994 0 0 1 3 21.007V4.993C3 4.445 3.445 4 3.993 4H7zm0 2H5v14h14V6h-2v2H7V6zm2-2v2h6V4H9z" />
-            </svg>
-          </span>
+        <p className="text-accented-low">
+          {copied ? "Copied to clipboard 🎉" : "Drop an email at 👇:"}
+        </p>
+        <div className="flex justify-center items-center gap-6 ">
+          <p className="inline">{email}</p>
+          <span onClick={copyEmailHandler}>{copied ? tickSVG : copySVG}</span>
         </div>
       </div>
     </section>
--- src/static/contactMe.tsx
+++ src/static/contactMe.tsx
@@ -29,6 +29,28 @@ const telegramSVG = (
   </svg>
 );
 
+export const copySVG = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className="fill-primary w-6 hover:fill-accented-low"
+  >
+    <path fill="none" d="M0 0h24v24H0z" />
+    <path d="M7 4V2h10v2h3.007c.548 0 .993.445.993.993v16.014a.994.994 0 0 1-.993.993H3.993A.994.994 0 0 1 3 21.007V4.993C3 4.445 3.445 4 3.993 4H7zm0 2H5v14h14V6h-2v2H7V6zm2-2v2h6V4H9z" />
+  </svg>
+);
+
+export const tickSVG = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className="fill-primary w-6 hover:fill-accented-high"
+  >
+    <path fill="none" d="M0 0h24v24H0z" />
+    <path d="M11.602 13.76l1.412 1.412 8.466-8.466 1.414 1.414-9.88 9.88-6.364-6.364 1.414-1.414 2.125 2.125 1.413 1.412zm.002-2.828l4.952-4.953 1.41 1.41-4.952 4.953-1.41-1.41zm-2.827 5.655L7.364 18 1 11.636l1.414-1.414 1.413 1.413-.001.001 4.951 4.951z" />
+  </svg>
+);
+
 export const contacts = [
   {
     title: "Linkedin",
