d57d26ecd0ece6e6f184769ccc3de9f1b7eb44c8
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-03-01 16:59:39 +0530

    switching vite config for resume

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import resume from "../static/resume.pdf";
+
 const Header = () => {
   return (
     <nav className="invisible sticky top-0 z-10 text-primary flex justify-between items-center bg-background bg-opacity-90 px-8 py-3 md:visible">
@@ -25,9 +27,7 @@ const Header = () => {
         </a>
         <a
           className="px-4 py-2 border rounded-md border-accented-high text-accented-low hover:bg-accented-dark duration-300"
-          href={
-            "https://github.com/Shubham052000/portfolio-web-app/blob/main/src/static/resume.pdf"
-          }
+          href={resume}
           target="_blank"
           rel="noopener noreferrer"
         >
