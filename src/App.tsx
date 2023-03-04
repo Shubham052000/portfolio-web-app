13ccb9b7108666c0b39d3be03729103484c37245
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-03-04 18:47:14 +0530

    removing TestComp references

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainPage from "./components/MainPage";
 import Projects from "./components/Projects";
-import TestComp from "./components/TestComp";
 
 function App() {
   return (
